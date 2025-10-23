594267b995bd4c2e2b002d6cf8b06714b24481a9
YLudo <ludovic.roux31@ynov.com>
2025-10-23 15:09:25 +0200

    feat: :sparkles: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,45 @@ const fontSans = FontSans({
     variable: "--font-sans",
 });
 
+const siteUrl = "https://cofound-app.com/"
+
 export const metadata: Metadata = {
     title: "CoFound",
-    description: "CoFound est un écosystème de co-création destinée en priorité aux jeunes, mais ouverte à toute personne souhaitant lancer ou rejoindre un projet.",
+    description: "La plateforme qui connecte porteurs de projets, développeurs, designers et autres. Trouvez vos co-fondateurs grâce à notre matching IA et donnez vie à vos idées de startup.",
+    openGraph: {
+        title: "CoFound : Trouvez vos associés et lancez votre projet",
+        description: "La plateforme pour transformer vos idées en startups à succès.",
+        url: siteUrl,
+        siteName: "CoFound",
+        images: [
+            {
+                url: `${siteUrl}/imports/logo.png`, 
+                width: 1200,
+                height: 630,
+                alt: "Illustration de la plateforme CoFound",
+            },
+        ],
+        locale: "fr_FR",
+        type: "website"
+    },
+    icons: {
+        icon: "/favico.ico",
+    },
+    metadataBase: new URL(siteUrl),
+    alternates: {
+        canonical: "/",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        }
+    }
 };
 
 export default function RootLayout({
